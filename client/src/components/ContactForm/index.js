5b22673627b13559d519ac79d1b557429bb039b0
fchoi1 <masterfabster2010@hotmail.com>
2022-06-13 19:50:53 -0400

    added functional email

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import './contactform.css';
+
+const USER_KEY = process.env.REACT_APP_USER_KEY;
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
 
 function ContactForm(props) {
   const [formState, setFormState] = useState({
@@ -8,28 +13,50 @@ function ContactForm(props) {
     message: ''
   });
 
-  console.log(formState);
   const onNameChange = (event) => {
-    setFormState({ name: event.target.value });
+    setFormState({ ...formState, name: event.target.value });
   };
 
   const onEmailChange = (event) => {
-    setFormState({ email: event.target.value });
+    setFormState({ ...formState, email: event.target.value });
   };
 
   const onMessageChange = (event) => {
-    setFormState({ message: event.target.value });
+    setFormState({ ...formState, message: event.target.value });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+    try {
+      const result = await emailjs.send(
+        SERVICE_ID,
+        TEMPLATE_ID,
+        formState,
+        USER_KEY
+      );
+
+      if (result) {
+        alert('Message Sent!');
+        setFormState({ name: '', email: '', message: '' });
+      } else {
+        alert('Message was not sent');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Message was not sent');
+    }
   };
 
   return (
     <form id="contact-form" onSubmit={handleSubmit} method="POST">
       <div className="form-group">
         <label htmlFor="name">Name</label>
-        <input type="text" className="form-control" onChange={onNameChange} />
+        <input
+          type="text"
+          className="form-control"
+          onChange={onNameChange}
+          value={formState.name}
+        />
       </div>
 
       <div className="form-group">
@@ -39,6 +66,7 @@ function ContactForm(props) {
           className="form-control"
           aria-describedby="emailHelp"
           onChange={onEmailChange}
+          value={formState.email}
         />
       </div>
 
@@ -48,6 +76,7 @@ function ContactForm(props) {
           className="form-control"
           rows="5"
           onChange={onMessageChange}
+          value={formState.message}
         ></textarea>
       </div>
       <button type="submit" className="btn btn-primary">
--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -4,6 +4,8 @@ import ContactForm from 'components/ContactForm';
 import './contact.css';
 
 function Contact(props) {
+
+
   return (
     <section id="Contact">
       <div className="section-wrapper contact-section">
